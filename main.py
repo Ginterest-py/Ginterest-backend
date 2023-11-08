dc983fa2d1112ae80af90bc6344efd69f66d1922
sh4lluloveme <wh3n_gg@kakao.com>
2023-11-08 12:15:58 +0900

    Feat: First commit 🥂

---
+++ main.py
@@ -0,0 +1,111 @@
+from flask import Flask, make_response, request
+from gauth_python.requests import token_issuance,token_reissuance
+import setting, modules
+from bson import json_util
+
+app = Flask(__name__)
+
+#return 대상을 함수로 변경하여금 응답 값을 유연하게 변경할 수 있도록 해야함, 리팩토링 필요
+
+#account
+@app.route('/api/users/login', methods=['POST'])
+def getCodeAndGenToken():
+    params = request.get_json()
+    user_token = token_issuance(
+        code = params['client_code'],
+        clientId = setting.gauth_clientId,
+        clientSecret = setting.gauth_clientSecret,
+        redirectUri = setting.gauth_redirectUri
+    )
+    if ((modules.getUserIdFromDb(modules.getUserEmail(user_token))) == None):
+        client_id = modules.writeUserDb(user_token)
+        return json_util.dumps((client_id, user_token,  False), default=json_util.default)
+    client_id = modules.getUserIdFromDb
+    return json_util.dumps((client_id, user_token, True), default=json_util.default)
+
+@app.route('/api/users/token', methods=['POST'])
+def getUserToken():
+    params = request.get_json()
+    return_token = token_reissuance(
+        refreshToken = params['client_refresh_token']
+    )
+    return json_util.dumps(return_token, default=json_util.default)
+
+@app.route('/api/users/nickname', methods=['POST'])
+def writeUserNickname():
+    params = request.get_json()
+    findUserByEmail = modules.getUserEmail(params['user_token'])
+    modules.changeUserNickname(findUserByEmail,params['nickname'])
+    return make_response("status 200", 200)
+
+@app.route('/api/user/<clientId>', methods=['GET'])
+def getUserInfo(clientId):
+    result = list(modules.getUserInfo(clientId))
+    return json_util.dumps(result, default=json_util.default)
+
+@app.route('/api/user/<clientId>/pins', methods=['GET'])
+def getOwnPins(clientId):
+    result = list(modules.getUserBoardDb(clientId))
+    return json_util.dumps(result, default=json_util.default)
+
+@app.route('/api/user/<clientId>/likes', methods=['GET'])
+def getLikedPosting(clientId):
+    result = list(modules.getLikedPostDb(clientId))
+    return json_util.dumps(result, default=json_util.default)
+
+#pins (board)
+@app.route('/api/pins', methods=['POST'])
+def writeUserPins():
+    params = request.get_json()
+    return json_util.dumps(modules.writeBoardDb(params['doc'], params['clientId'], "board", "0", "0") == True)
+
+@app.route('/api/pins', methods=['GET'])
+def getBoardPins():
+    result = list(modules.getBoardDb())
+    return json_util.dumps(result, default=json_util.default)
+
+@app.route('/api/pins/<pinId>', methods=['GET'])
+def getUserPins(pinId):
+    result = list(modules.getPostDb(post_id=pinId))
+    return json_util.dumps(result, default=json_util.default)
+
+@app.route('/api/pins/<pinId>', methods=['PUT'])
+def putPostEdit(pinId):
+    data = request.get_json()
+    if (modules.writeBoardDb(data.doc, data.clientId, data.type, data.post_id, pinId) == False):
+        return make_response("status 400", 400)
+    return make_response("status 200", 200)
+
+@app.route('/api/pins/search', methods=['GET'])
+def getUserSearchPins():
+    keywords = request.args.get('keywords')
+    result = list(modules.getPostDb(keyword=keywords))
+    return json_util.dumps(list(result), default=json_util.default)
+
+# @app.route('/api/pins/<pinId>/likes', methods=['POST'])
+# def postPostLikes(pinId):
+#     clientId = request.args.get('clientId')
+#     if (modules.putPostLikeDb(pinId,clientId) == False):
+#         return make_response("status 400", 400)
+#     return make_response("status 200", 200)
+
+# @app.route('/api/pins/<pinId>/dislikes', methods=['POST'])
+# def postPostdisLikes(pinId):
+#     clientId = request.args.get('clientId')
+#     if (modules.deletePostLikeDb(pinId,clientId) == False):
+#         return make_response("status 400", 400)
+#     return make_response("status 200", 200)
+
+
+@app.route('/api/pins/<pinId>', methods=['DELETE'])
+def deletePostLikes(pinId):
+    modules.deletePostDb(pinId)
+    return make_response("status 200", 200)
+
+@app.route('/api/images', methods=['POST'])
+def upload_profile_picture():
+    params = request.get_json()
+    modules.uploadFile(request,params['clientId'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ modules.py
@@ -0,0 +1,166 @@
+from gauth_python.requests import user_info
+from pymongo import MongoClient
+import setting
+import datetime, secrets, os
+from bson.objectid import ObjectId
+
+client = MongoClient(setting.MONGO_URL)
+db = client['event']
+nowtime = datetime.datetime.now()
+
+#account
+def getUserEmail(user_token):
+    UserInfo = user_info(
+        accessToken = user_token
+    )
+    return UserInfo.email
+
+def writeUserDb(user_token):
+    UserInfo = user_info(
+        accessToken = user_token
+    )
+    doc = {
+        'clientId' : secrets.token_hex(nbytes=40),
+        'email' : UserInfo.email,
+        'liked_post' : [],
+        'created_at': datetime.datetime.utcnow()
+    }
+    db.users.insert_one(doc)
+    return doc.clientId
+
+def changeUserNickname(user_email,nickname):
+    db.users.update_one({'email':f'{user_email}'},{'$set':{'nickname':f'{nickname}'}})
+    return True
+
+def getUserIdFromDb(user_email):
+    user_info = db.userinfo.find({'user_email':user_email})
+    return user_info['clientId']
+
+def getUserInfo(clientId):
+    user_info = db.users.find_one({'clientId':f"{clientId}"})
+    return user_info
+
+def checkUserExists(userId):
+    user_info = db.userinfo.find({'clientId':userId})
+    if user_info != None:
+        return True
+    else:
+        return False 
+    
+def getLikedPostDb(clientId):
+    if (checkUserExists(clientId) == False):
+        return False
+    doc = db.userinfo.find({'clientId':clientId})
+    return doc['liked_post']
+
+# board
+
+def checkownPost(clientId, post_id):
+    if (checkUserExists(clientId) == False):
+        return False
+    doc = db.globalboard.find_one({'_id':ObjectId(f'{post_id}')})
+    if doc['clientId'] == clientId:
+        return True
+    else:
+        return False
+
+def getUserBoardDb(clientId):
+    if (checkUserExists(clientId) == False):
+        return False
+    user_info = db.globalboard.find({'clientId':clientId})
+    return user_info
+
+def putPostLikeDb(post_id, clientId):
+    if (checkUserExists(clientId) == False and checkownPost(clientId,post_id)):
+        return False
+    doc = db.globalboard.find_one_and_update({'post_id':post_id},{'$inc':{ 'post_like': 1}})
+    db.userinfo.update_one({'clientId':clientId},{'$push':{'liked_post':post_id}})
+    return True
+
+def deletePostLikeDb(post_id, clientId):
+    if (checkUserExists(clientId) == False and checkownPost(clientId,post_id)):
+        return False
+    doc = db.globalboard.find_one_and_update({'post_id':post_id},{'$inc':{ 'post_like': -1}})
+    client = db.userinfo.update_one({'clientId':clientId},{'$pull':{'liked_post':post_id}})
+    if ((doc | client) == None |"null"):
+        return False
+    return True
+
+def getBoardDb():
+    board_doc = db.globalboard.find({})
+    return board_doc
+
+def getPostDb(keyword=None, post_id=None):
+    board_doc = db.globalboard.find_one({'_id':ObjectId(f'{post_id}')}) if post_id is not None else db.globalboard.find({'detail': {'$regex': f'{keyword}'}})
+    return board_doc
+
+def deletePostDb(post_id,clientId):
+    if (checkownPost(clientId,post_id) == True):
+        board_doc = db.globalboard.find_one_and_delete({'_id':ObjectId(f'{post_id}')})
+    elif (board_doc == None |"null"):
+        return False
+    else:
+        return False
+    return True
+
+def writeBoardDb(doc, clientId, type, post_id, ref_id):
+    if checkUserExists(client) == False:
+        return False
+    doc = {
+            'date' : nowtime.year+nowtime.month+nowtime.day,
+            'detail' : doc,
+            'clientId': clientId,
+            'like_count': 0
+    }
+    if type == "comment":
+        doc['post_id'] = post_id
+        db.globalboardcomment.insert_one(doc)
+    elif type == "recomment":
+        doc['post_id'] = post_id
+        doc['ref_comment_id'] = ref_id
+        db.globalboardrecomment.insert_one(doc)
+    elif type == "board":
+        db.globalboard.insert_one(doc)
+    elif type == "update":
+        post_id = ref_id
+        db.globalboard.update_one({'_id':ObjectId(f'{post_id}')},{'$set':{'detail':f'{doc}'}})
+
+    return True
+
+# def getCommentDb(post_id):
+#     comment_doc = db.globalboardcomment.find({'post_id':post_id}) + db.globalboardrecomment.find({'post_id':post_id})
+#     return comment_doc
+
+# def getCommentDb(post_id):
+#     comment_doc = db.globalboardcomment.find({'post_id':post_id}) + db.globalboardrecomment.find({'post_id':post_id})
+#     return comment_doc
+
+# def getUserCommentDb(clientId):
+#     user_info = db.globalboardrecomment.find({'clientId':clientId}) + db.globalboardcomment.find({'clientId':clientId})
+#     return user_info
+
+# --
+
+def uploadFile(request,clientId):
+    if (checkUserExists(clientId) == False):
+        return False
+    if 'file' not in request.files:
+        return "파일을 선택해주세요."
+
+    file = request.files['file']
+    if file.filename == '':
+        return False
+    if checkUserExists(client) == False:
+        return False
+    if file:
+        filename = f"{secrets.token_hex(10)}"
+        file.save(os.path.join(setting.uploadpat, filename))
+
+        # MongoDB에 파일 정보 저장
+        image_info = {
+            "filename": filename,
+            "clientId": clientId
+        }
+
+        result = db.images.insert_one(image_info)
+        return result.inserted_id
